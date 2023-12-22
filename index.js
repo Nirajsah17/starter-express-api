70e06dcbd6213e404cd0bd34a48db6ded878a7fb
niraj <nirajnsp5@gmail.com>
2023-12-23 00:42:43 +0530

    whoami end point added

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/whoami',(req,res)=>{
+    res.send("Hello I am signaling server");
+})
 app.listen(process.env.PORT || 3000)
